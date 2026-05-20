51b7a0ae4cc9a2643ed9945be0d5da4f3e568699
Giulia Caglia <cagliagiulia42@gmail.com>
2026-05-20 08:10:46 +0200

    WIP: Single item scraper

--- solutions/books_scraper/books_scraper/spiders/books.py
+++ solutions/books_scraper/books_scraper/spiders/books.py
@@ -4,7 +4,9 @@ import scrapy
 class BooksSpider(scrapy.Spider):
     name = "books"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com"]
+    start_urls = [
+        "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    ]
 
     def parse(self, response):
         pass
--- solutions/main.py
+++ solutions/main.py
@@ -1,5 +1,13 @@
+from books_scraper.books_scraper.spiders.books import BooksSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+
 def main():
-    print("Hello from solutions!")
+    settings = get_project_settings("books_scraper/settings.py")
+    process = CrawlerProcess(settings=settings)
+    process.crawl(BooksSpider)
+    process.start()
 
 
 if __name__ == "__main__":
